9c6396f0ebe8dc2f022cf2c3fbaaf922c5a6b2e8
th3093 <yannick.theobald@gmail.com>
2023-05-13 19:21:27 +0200

    merge ready for v2

--- contracts/TrustFund.sol
+++ contracts/TrustFund.sol
@@ -5,36 +5,42 @@ pragma solidity <0.9.0;
 contract TrustFund{
 
     struct Beneficiary{
-        address spender;
         uint value;
         uint payOutDate;
-        bool paid;
+        uint32 paid;
+        address[] spenders;
     }
 
-//TODO: mapping für mehr als nur den contract deployer -> jeder kann nutzen!!
-    mapping(address => mapping(address => bool)) spenders;
-    mapping(address =>  Beneficiary) public beneficiaries;
+
+    mapping(address => bytes32) beneficiaryIDs;
+    mapping(bytes32 => Beneficiary) beneficiaries;
+    mapping(address => mapping(bytes32 => bool)) spenderVal;
 
 
     modifier isSpender(address _ben){
-        require(spenders[msg.sender][_ben], "Origin of call not spender address");
+        require(spenderVal[msg.sender][beneficiaryIDs[_ben]], "Origin of call not initial spender address");
+        _;
+    }
+
+    modifier isValidValue(){
+        require(msg.value > 0, "Please send some funds");
         _;
     }
 
     modifier isBen(address _ben) {
-        require(beneficiaries[_ben].value > 0, "Please add beneficiary address first");
+        require(beneficiaries[beneficiaryIDs[_ben]].value > 0, "Please add beneficiary address first");
         _;
     }
 
     modifier isNotBen(address _ben){
-        require(beneficiaries[_ben].value == 0, "Beneficiary already created. Check etherscan for more information.");
+        require(beneficiaries[beneficiaryIDs[_ben]].value == 0, "Beneficiary already created. Check etherscan for more information.");
         _;
     }
 
     modifier isWithdrawable(address _ben){
-        require(beneficiaries[_ben].value > 0, "Nothing to Withdraw. Please add funds first");
-        require(beneficiaries[_ben].payOutDate < block.timestamp, "Not withdrawable. Wait till fund locking period ends");
-        require(beneficiaries[_ben].paid == false, "Already withdrawn funds");
+        require(beneficiaries[beneficiaryIDs[_ben]].value > 0, "Nothing to Withdraw. Please add funds first");
+        require(beneficiaries[beneficiaryIDs[_ben]].payOutDate < block.timestamp, "Not withdrawable. Wait till fund locking period ends");
+        require(beneficiaries[beneficiaryIDs[_ben]].paid == false, "Already withdrawn funds");
         _;
     }
 
@@ -62,31 +68,49 @@ contract TrustFund{
         uint initialStamp
     );
 
+
     // beneficiaries[_ben] = Beneficiary(msg.value, _timeToPayOut * 31556952 + block.timestamp, false);
-    function addBeneficiary(address _ben, uint _timeToPayOut) external payable isNotBen(_ben) {
-        beneficiaries[_ben] = Beneficiary(msg.sender, msg.value, _timeToPayOut * 20 + block.timestamp, false);
-        spenders[msg.sender][_ben] = true;
+    function addBeneficiary(address _ben, uint _timeToPayOut) external payable isValidValue() isNotBen(_ben) {
+        beneficiaryIDs[_ben] = keccak256(abi.encodePacked(_ben));
+        beneficiaries[beneficiaryIDs[_ben]] = Beneficiary(msg.value, _timeToPayOut * 20 + block.timestamp, false, new address[](0));
+        beneficiaries[beneficiaryIDs[_ben]].spenders.push(msg.sender);
+        spenderVal[msg.sender][beneficiaryIDs[_ben]] = true;
         emit BeneficiaryCreated(msg.sender, _ben, _timeToPayOut, msg.value);
     }
 
-    function deposit(address _ben) external payable isSpender(_ben) isBen(_ben) {
-        beneficiaries[_ben].value += msg.value;
+    function safeDeposit(address _ben) external payable isSpender(_ben) isBen(_ben) {
+        beneficiaries[beneficiaryIDs[_ben]].value += msg.value;
+        emit Deposit(_ben, msg.value);
+    }
+
+    function forceDeposit(address _ben) external payable isBen(_ben) {
+        beneficiaries[beneficiaryIDs[_ben]].value += msg.value;
+        beneficiaries[beneficiaryIDs[_ben]].spenders.push(msg.sender);
+        spenderVal[msg.sender][beneficiaryIDs[_ben]] = true;
         emit Deposit(_ben, msg.value);
     }
 
     function withdraw() external isWithdrawable(msg.sender){
-        Beneficiary storage ben = beneficiaries[msg.sender];
+        Beneficiary storage ben = beneficiaries[beneficiaryIDs[msg.sender]];
         ben.paid = true;
         payable(msg.sender).transfer(ben.value);
         emit Withdraw(msg.sender, ben.value);
-        delete beneficiaries[msg.sender];
-        delete spenders[ben.spender][msg.sender];
+        delete beneficiaries[beneficiaryIDs[msg.sender]];
+        delete beneficiaryIDs[msg.sender];
     }
   
     function remainingTime() external view returns(uint){
-        //emit Remaining(msg.sender, beneficiaries[msg.sender].payOutDate > block.timestamp ? true:false, block.timestamp, beneficiaries[msg.sender].payOutDate);
-        require(beneficiaries[msg.sender].payOutDate > block.timestamp, "Funds are already unlocked and can be claimed.");
-        return beneficiaries[msg.sender].payOutDate - block.timestamp;
+        require(beneficiaries[beneficiaryIDs[msg.sender]].payOutDate > block.timestamp, "Funds are already unlocked and can be claimed.");
+        return beneficiaries[beneficiaryIDs[msg.sender]].payOutDate - block.timestamp;
+    }
+
+    function getBen(address _ben) external view returns(uint, uint, bool, address[] memory){
+        return (
+            beneficiaries[beneficiaryIDs[_ben]].value, 
+            beneficiaries[beneficiaryIDs[_ben]].payOutDate, 
+            beneficiaries[beneficiaryIDs[_ben]].paid, 
+            beneficiaries[beneficiaryIDs[_ben]].spenders
+            );
     }
 
 }
